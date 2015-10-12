78546064b096141b0951456955234b5886de3f0b
吴添祥 <funiflying@163.com>
2015-10-12 15:42:13 +0800

    ajax's post and get

--- base.js
+++ base.js
@@ -33,46 +33,102 @@ Base.Logger = {
       cookies
      ************************/
 Base.Cookie = {
-    setCookie: function (COOKIE_NAME, COOKIE_VALUE, OPTIONS) {
-        if (typeof COOKIE_VALUE != "undefined") {
-            var options = OPTIONS || {};
-            var date;
-            var expires = '';
-            if (COOKIE_VALUE === null) {
-                COOKIE_VALUE = '';
-                options.expires = -1;
+        /*
+         *@param:name
+         *@param:value
+         *@param:options
+         *@return:cookie
+         */
+        setCookie: function (COOKIE_NAME, COOKIE_VALUE, OPTIONS) {
+            if (typeof COOKIE_VALUE != "undefined") {
+                var options = OPTIONS || {};
+                var date;
+                var expires = '';
+                if (COOKIE_VALUE === null) {
+                    COOKIE_VALUE = '';
+                    options.expires = -1;
+                }
+                if (options.expires && typeof options.expires == "number") {
+                    date = new Date();
+                    date.setDate(date.getDate() + options.expires)
+                    expires = ';expires=' + date;
+                }
+                var path = options.path ? ';path=' + options.path : '';
+                var domain = options.domain ? '; domain=' + (options.domain) : '';
+                var secure = options.secure ? '; secure' : '';
+                document.cookie = [COOKIE_NAME, '=', encodeURIComponent(COOKIE_VALUE), expires, path, domain, secure].join('');
             }
-            if (options.expires && typeof options.expires == "number") {
-                date = new Date();
-                date.setDate(date.getDate() + options.expires)
-                expires = ';expires=' + date;
-            }
-            var path = options.path ? ';path=' + options.path : '';
-            var domain = options.domain ? '; domain=' + (options.domain) : '';
-            var secure = options.secure ? '; secure' : '';
-            document.cookie = [COOKIE_NAME, '=', encodeURIComponent(COOKIE_VALUE), expires, path, domain, secure].join('');
-        }
-    },
-    getCookie: function (COOKIE_NAME) {
-        if (document.cookie && document.cookie != "") {
-            var COOKIE_VALUE = null;
-            var cookies = document.cookie.split(';');
+        },
+        /*
+         *@param:name
+         *@return:value
+         */
+        getCookie: function (COOKIE_NAME) {
+            if (document.cookie && document.cookie != "") {
+                var COOKIE_VALUE = null;
+                var cookies = document.cookie.split(';');
 
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = cookies[i].replace(/(^\s*)|(\s*$)/g, "");
-                if (cookie.substring(0, COOKIE_NAME.length + 1) == COOKIE_NAME + '=') {
-                    COOKIE_VALUE = encodeURIComponent(cookie.substring(COOKIE_NAME.length + 1));
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i].replace(/(^\s*)|(\s*$)/g, "");
+                    if (cookie.substring(0, COOKIE_NAME.length + 1) == COOKIE_NAME + '=') {
+                        COOKIE_VALUE = encodeURIComponent(cookie.substring(COOKIE_NAME.length + 1));
+                    }
+                }
+                return COOKIE_VALUE;
+            }
+        },
+        /*
+         *@param:name
+         *@return:void
+         */
+        clearCookie: function (COOKIE_NAME) {
+            var cookie = this.getCookie(COOKIE_NAME);
+            if (cookie != null) {
+                this.setCookie(COOKIE_NAME, null);
+            }
+        }
+    }
+    /************************
+     Ajax
+    ************************/
+Base.Ajax = {
+    getAjaxHttp: function () {
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject('Microsoft.XMLHTTp');
+        }
+        return xhr;
+    },
+    ajaxRequest: function (url, methodType, async, params, callback) {
+        var xhr = this.getAjaxHttp();
+        xhr.open(methodType, url, async);
+        if (methodType == 'post') {
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(params);
+        }
+        else{
+            xhr.send(null);  
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    callback.call(this,xhr.responseText);
                 }
             }
-            return COOKIE_VALUE;
         }
     },
-    clearCookie: function (COOKIE_NAME) {
-        var cookie = this.getCookie(COOKIE_NAME);
-        if (cookie != null) {
-            this.setCookie(COOKIE_NAME, null);
-        }
-
-
+    post:function(url, async, params, callback){
+        this.ajaxRequest(url,'post', async, params, callback)
+    },
+    get:function(url, async,callback){
+        this.ajaxRequest(url,'get', async, '', callback)
     }
+
+
+
+
+
+
 }
